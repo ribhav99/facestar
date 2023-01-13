d03bda681af56fc94f3453197b148122c2d8be73
ribhav99 <38373725+ribhav99@users.noreply.github.com>
2023-01-13 17:01:05 -0600

    file to compute gaze directions

---
+++ gaze_direction.py
@@ -0,0 +1,23 @@
+import os
+from pqdm.processes import pqdm
+import multiprocessing
+
+if not os.path.isdir('gaze_direction_train'):
+    os.mkdir('gaze_direction_train')
+if not os.path.isdir('gaze_direction_test'):
+    os.mkdir('gaze_direction_test')
+
+def run_gaze_estimation(folder, f):
+    cmd = f'python3 ../gazeEstimation/ptgaze/__main__.py --mode eth-xgaze --video {os.path.join(folder, f)} -o gaze_direction_{folder}'
+    os.system(cmd)
+
+args = []
+for f in os.listdir('train'):
+    d = {'folder': 'train', 'f': f}
+    args.apped(d)
+
+for f in os.listdir('test'):
+    d = {'folder': 'test', 'f': f}
+    args.apped(d)
+
+result = pqdm(args, run_gaze_estimation, n_jobs=multiprocessing.cpu_count(), argument_type='kwargs')
