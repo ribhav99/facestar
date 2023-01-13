f2326ff5d092d5a41257fd622b42ded0fe5b4063
ribhav99 <38373725+ribhav99@users.noreply.github.com>
2023-01-13 17:20:41 -0600

    added mp4 as extension and removed on screen show

--- gaze_direction.py
+++ gaze_direction.py
@@ -9,12 +9,12 @@ if not os.path.isdir('gaze_direction_test'):
 args = []
 folder = 'train'
 for f in os.listdir('train'):
-    cmd = f'python3 ../gazeEstimation/ptgaze/__main__.py --mode eth-xgaze --video {os.path.join(folder, f)} -o gaze_direction_{folder} >> /dev/null'
+    cmd = f'python3 ../gazeEstimation/ptgaze/__main__.py --mode eth-xgaze --video {os.path.join(folder, f)} -o gaze_direction_{folder} --no-screen -e mp4'
     args.append(cmd)
 
 folder = 'test'
 for f in os.listdir('test'):
-    cmd = f'python3 ../gazeEstimation/ptgaze/__main__.py --mode eth-xgaze --video {os.path.join(folder, f)} -o gaze_direction_{folder} >> /dev/null'
+    cmd = f'python3 ../gazeEstimation/ptgaze/__main__.py --mode eth-xgaze --video {os.path.join(folder, f)} -o gaze_direction_{folder} --no-screen -e mp4'
     args.append(cmd)
 
 for c in tqdm(args):
