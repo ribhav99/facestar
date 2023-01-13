420d7b600470ecf51a851f73959c238e9cd285ab
ribhav99 <38373725+ribhav99@users.noreply.github.com>
2023-01-13 17:10:57 -0600

    redirecting output to /dev/null

--- gaze_direction.py
+++ gaze_direction.py
@@ -6,19 +6,15 @@ if not os.path.isdir('gaze_direction_train'):
 if not os.path.isdir('gaze_direction_test'):
     os.mkdir('gaze_direction_test')
 
-def run_gaze_estimation(folder, f):
-    cmd = f'python3 ../gazeEstimation/ptgaze/__main__.py --mode eth-xgaze --video {os.path.join(folder, f)} -o gaze_direction_{folder}'
-    os.system(cmd)
-
 args = []
 folder = 'train'
 for f in os.listdir('train'):
-    cmd = f'python3 ../gazeEstimation/ptgaze/__main__.py --mode eth-xgaze --video {os.path.join(folder, f)} -o gaze_direction_{folder}'
+    cmd = f'python3 ../gazeEstimation/ptgaze/__main__.py --mode eth-xgaze --video {os.path.join(folder, f)} -o gaze_direction_{folder} >> /dev/null'
     args.append(cmd)
 
 folder = 'test'
 for f in os.listdir('test'):
-    cmd = f'python3 ../gazeEstimation/ptgaze/__main__.py --mode eth-xgaze --video {os.path.join(folder, f)} -o gaze_direction_{folder}'
+    cmd = f'python3 ../gazeEstimation/ptgaze/__main__.py --mode eth-xgaze --video {os.path.join(folder, f)} -o gaze_direction_{folder} >> /dev/null'
     args.append(cmd)
 
 for c in tqdm(args):
