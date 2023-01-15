85835e24f115c367fe6b7c085dc0e1833cd020b5
ribhav99 <38373725+ribhav99@users.noreply.github.com>
2023-01-15 16:18:33 -0600

    update

---
+++ gaze_estimation.py
@@ -0,0 +1,4 @@
+import os
+
+cmd = 'python3 ../gazeEstimation/ptgaze/__main__.py --mode eth-xgaze --video train/female_session1_fps20.avi -o temp --no-screen --fps 20 --gaze_array 539 633 537 650 493 623 479 651 514 625 493 646 479 618 477 633 709 683 699 707'
+os.system(cmd)
